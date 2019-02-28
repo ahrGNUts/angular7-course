15135c20e5c6baf8ff2ae0e67031dfd894541fe2
Patrick <revmydev@gmail.com>
2019-02-28 10:58:22 -0800

    added CLI-generated ServersComponent

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,17 @@
+// the ServersComponent was created with the CLI command
+// ng generate component servers (or ng g c servers)
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
